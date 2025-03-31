3068dd4f6a16b13b0b2f9d442941695e881b3812
domyeong <wjsehaud9413@naver.com>
2025-03-31 20:29:15 +0900

    feat: basic normalizeVNode 테스트 구현

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,25 @@
+function normalizeVNodeChildren(children) {
+  return children.map((node) => normalizeVNode(node)).filter(Boolean);
+}
+
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return "";
+  }
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+  if ("type" in vNode && typeof vNode.type === "function") {
+    return normalizeVNode(
+      vNode.type({
+        ...vNode.props,
+        children: normalizeVNodeChildren(vNode.children),
+      }),
+    );
+  }
+
+  return {
+    ...vNode,
+    children: normalizeVNodeChildren(vNode.children),
+  };
 }
