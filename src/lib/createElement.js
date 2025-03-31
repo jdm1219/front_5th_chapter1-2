40789bf0a9000ed519ef70b7a7f3f27e12e4b313
domyeong <wjsehaud9413@naver.com>
2025-03-31 21:38:45 +0900

    feat: basic createElement 테스트 구현

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,32 @@
-import { addEvent } from "./eventManager";
+export function createElement(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return document.createTextNode("");
+  }
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(String(vNode));
+  }
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    vNode.forEach((node) => fragment.appendChild(createElement(node)));
+    return fragment;
+  }
 
-export function createElement(vNode) {}
+  const { type, props, children } = vNode;
+  if (typeof type !== "string") throw new Error();
 
-function updateAttributes($el, props) {}
+  const element = document.createElement(type);
+  if (props) updateAttributes(element, props);
+  children.forEach((child) => element.appendChild(createElement(child)));
+
+  return element;
+}
+
+function updateAttributes($el, props) {
+  for (const [key, value] of Object.entries(props)) {
+    if (key === "className") {
+      $el.setAttribute("class", value);
+      return;
+    }
+    $el.setAttribute(key, value);
+  }
+}
