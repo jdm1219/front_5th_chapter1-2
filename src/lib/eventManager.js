5c0e0a806b6a1bb2b4f23c1f11ec843be8e9ddc8
domyeong <wjsehaud9413@naver.com>
2025-03-31 22:53:22 +0900

    feat: basic eventManager 테스트 구현

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,32 @@
-export function setupEventListeners(root) {}
+const eventMap = new Map();
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  eventMap.forEach((handlers, eventType) =>
+    root.addEventListener(eventType, (e) => {
+      handlers.forEach(([element, handler]) => {
+        if (element.contains(e.target)) {
+          handler(e);
+        }
+      });
+    }),
+  );
+}
 
-export function removeEvent(element, eventType, handler) {}
+export function addEvent(element, eventType, handler) {
+  if (!eventMap.get(eventType)) {
+    eventMap.set(eventType, []);
+  }
+  eventMap.get(eventType).push([element, handler]);
+}
+
+export function removeEvent(element, eventType, handler) {
+  const eventHandlers = eventMap.get(eventType);
+  if (!eventHandlers) return;
+  const targetEventIndex = eventHandlers.findIndex((handlers) => {
+    const [_element, _handler] = handlers;
+    return element === _element && handler === _handler;
+  });
+  if (targetEventIndex === -1) return;
+  eventHandlers.splice(targetEventIndex, 1);
+  if (eventHandlers.length === 0) eventMap.delete(eventType);
+}
